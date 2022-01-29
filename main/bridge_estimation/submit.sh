2c2d3447bae3d2253a188c4d8e70d984063f48f8
Frederik-DTU <s164222@student.dtu.dk>
2022-01-30 00:36:22 +0100

    Update

--- main/bridge_estimation/submit.sh
+++ main/bridge_estimation/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #BSUB -q hpc
-#BSUB -J simple_ahs_theta
+#BSUB -J cc_ahs_theta
 #BSUB -R "span[hosts=1]"
 #BSUB -n 1 
-#BSUB -W 5:00
+#BSUB -W 24:00
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -u s164222@student.dtu.dk
 #BSUB -o HPC_output/output_%J.out
@@ -14,8 +14,8 @@
 #Load the following in case
 module swap python3/3.8.2
 
-python3 simple_data.py \
-    --save_path simple_models/ \
+python3 corpus_callosum.py \
+    --save_path corpus_callosum_models/ \
     --model ahs \
     --eta 0.98 \
     --delta 0.001 \
--- main/template_estimation/submit.sh
+++ main/template_estimation/submit.sh
@@ -3,7 +3,7 @@
 #BSUB -J cc_ahs_theta
 #BSUB -R "span[hosts=1]"
 #BSUB -n 1 
-#BSUB -W 5:00
+#BSUB -W 24:00
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -u s164222@student.dtu.dk
 #BSUB -o HPC_output/output_%J.out
